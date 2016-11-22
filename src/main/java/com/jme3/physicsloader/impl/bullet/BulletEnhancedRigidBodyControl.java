2baee96a11a2c8bdba10ec185b918bb7bb87f7d5
Riccardo Balbo <riccardo@forkforge.net>
2016-11-22 23:56:10 +0100

    Enhanced BulletEnhancedRigidBodyControl

--- src/main/java/com/jme3/physicsloader/impl/bullet/BulletEnhancedRigidBodyControl.java
+++ src/main/java/com/jme3/physicsloader/impl/bullet/BulletEnhancedRigidBodyControl.java
@@ -9,9 +9,17 @@ import com.jme3.math.Matrix3f;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.physicsloader.EnhancedRigidBodyControl;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.control.Control;
 
 public class BulletEnhancedRigidBodyControl extends RigidBodyControl implements EnhancedRigidBodyControl{
-	protected AtomicBoolean locationUpdatedFromPhysicsK=new AtomicBoolean(false),rotationUpdatedFromPhysicsK=new AtomicBoolean(false),updateTransformFromSpatial=new AtomicBoolean(false);
+	/** Features
+	  Allow spatial to be moved with setLocal* methods when detached from ps, useful when contained in nodes
+	  Prevent bulletappstate from resetting gravity settings applied before the rb was attached to ps
+	  Allow kinematic rb to be moved with setLocal* methods.
+	 */
+	protected AtomicBoolean locationUpdatedFromPhysicsK=new AtomicBoolean(false),rotationUpdatedFromPhysicsK=new AtomicBoolean(false);
+	protected boolean updateGravity=false,updateTransformFromSpatial=false;
 
 	public BulletEnhancedRigidBodyControl(float mass){
 		super(mass);
@@ -28,7 +36,7 @@ public class BulletEnhancedRigidBodyControl extends RigidBodyControl implements 
 	@Override
 	public BulletEnhancedRigidBodyControl jmeClone() {
 		BulletEnhancedRigidBodyControl control=new BulletEnhancedRigidBodyControl(collisionShape,mass);
-		control.spatial=this.spatial;
+		control.setSpatial(spatial);
 		control.setAngularFactor(getAngularFactor());
 		control.setAngularSleepingThreshold(getAngularSleepingThreshold());
 		control.setCcdMotionThreshold(getCcdMotionThreshold());
@@ -52,12 +60,62 @@ public class BulletEnhancedRigidBodyControl extends RigidBodyControl implements 
 		return control;
 	}
 
+//    public Object getUserObject() {
+//    	return spatial;
+//    }
+
+    @Override
+    public Control cloneForSpatial(Spatial spatial) {
+    	BulletEnhancedRigidBodyControl control=new BulletEnhancedRigidBodyControl(collisionShape,mass);
+		control.setSpatial(spatial);
+		control.setAngularFactor(getAngularFactor());
+		control.setAngularSleepingThreshold(getAngularSleepingThreshold());
+		control.setCcdMotionThreshold(getCcdMotionThreshold());
+		control.setCcdSweptSphereRadius(getCcdSweptSphereRadius());
+		control.setCollideWithGroups(getCollideWithGroups());
+		control.setCollisionGroup(getCollisionGroup());
+		control.setDamping(getLinearDamping(),getAngularDamping());
+		control.setFriction(getFriction());
+		control.setGravity(getGravity());
+		control.setKinematic(isKinematic());
+		control.setKinematicSpatial(isKinematicSpatial());
+		control.setLinearSleepingThreshold(getLinearSleepingThreshold());
+		control.setPhysicsLocation(getPhysicsLocation(null));
+		control.setPhysicsRotation(getPhysicsRotationMatrix(null));
+		control.setRestitution(getRestitution());
+		if(mass>0){
+			control.setAngularVelocity(getAngularVelocity());
+			control.setLinearVelocity(getLinearVelocity());
+		}
+		control.setApplyPhysicsLocal(isApplyPhysicsLocal());
+		return control;
+    }
+	
+	protected Vector3f GRAVITY=null;
+	
+	public boolean hasCustomGravity(){
+		return GRAVITY!=null;
+	}
+	
 	@Override
 	public void setPhysicsSpace(PhysicsSpace space) {
 		super.setPhysicsSpace(space);
 		if(space!=null&&spatial!=null){
-			updateTransformFromSpatial.set(true);
+			updateTransformFromSpatial=true; 
+			updateGravity=true;
 		}
+	}
+	
+	@Override
+	public Vector3f getGravity(){
+		if(space==null)return GRAVITY;
+		else return super.getGravity();
+	}
+	
+	@Override
+	public void setGravity(Vector3f force){
+		GRAVITY=force;
+		if(space!=null)super.setGravity(force);
 	}
 
 	private Vector3f getSpatialTranslation() {
@@ -94,15 +152,23 @@ public class BulletEnhancedRigidBodyControl extends RigidBodyControl implements 
 			rotationUpdatedFromPhysicsK.set(true);
 		}
 	}
+	
+	
 
 	@Override
 	public void update(float tpf) {
 		if(space==null) return;
 
 		if(enabled&&spatial!=null){
-			if(updateTransformFromSpatial.getAndSet(false)){
+			if(updateGravity){
+				if(GRAVITY!=null)	super.setGravity(GRAVITY);
+				updateGravity=false;
+			}
+			
+			if(updateTransformFromSpatial){
 				setPhysicsLocation(getSpatialTranslation());
 				setPhysicsRotation(getSpatialRotation());
+				updateTransformFromSpatial=false;
 			}
 
 			if(isKinematic()&&kinematicSpatial){
